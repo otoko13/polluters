50c12f9766a5296e8cb48489755b240dce21a95f
Mayur Bapodra <otoko13@users.noreply.github.com>
2020-05-22 18:48:33 +0100

    initial commit

--- src/_components/worldMap/WorldMap.tsx
+++ src/_components/worldMap/WorldMap.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './worldMap.scss';
-import { ComposableMap, Geographies, Geography, ZoomableGroup, Position } from 'react-simple-maps';
+import { ComposableMap, Geographies, Geography, ZoomableGroup, Position, Point } from 'react-simple-maps';
 
 import mapJson from '../../data/world-110m.json';
 import { IPolluter } from '../../model/Polluter';
 import MapMarker from '../mapMarker/MapMarker';
-import InfoPanel from '../infoPanel/InfoPanel';
 
 export interface IWorldMapProps {
   polluters: IPolluter[];
@@ -19,6 +18,13 @@ export interface IWorldMapProps {
 
 const WorldMap = (props: IWorldMapProps) => {
   const [zoomLevel, setZoomLevel] = React.useState(1);
+  const [center, setCenter] = React.useState<Point>([60, 0]);
+
+  React.useEffect(() => {
+    if (props.selectedPolluter) {
+      setCenter([props.selectedPolluter.longitude + 60, 0]);
+    }
+  }, [props.selectedPolluter]);
 
   function shouldUnhighlightPolluter(polluter: IPolluter) {
     return (props.hoveredPolluters.length > 0 && !isHovered(polluter)) ||
@@ -29,9 +35,9 @@ const WorldMap = (props: IWorldMapProps) => {
     return props.hoveredPolluters.findIndex(p => p.rank === polluter.rank) > -1;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function handleZoom(position: Position) {
     setZoomLevel(position.zoom);
+    setCenter([position.x, position.y]);
   }
 
   const sortedPolluters = [...props.polluters].sort((p1, p2) => {
@@ -44,7 +50,7 @@ const WorldMap = (props: IWorldMapProps) => {
   return (
     <div className="WorldMap">
       <ComposableMap height={462}>
-        <ZoomableGroup center={[60, 0]} onMoveEnd={handleZoom}>
+        <ZoomableGroup center={center} zoom={zoomLevel} onMoveEnd={handleZoom}>
           <Geographies geography={mapJson}>
             {
               ({ geographies }): JSX.Element[] => geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
